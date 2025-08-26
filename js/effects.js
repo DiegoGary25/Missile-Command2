238494095e2ac532249c3efcaef2f2b59498e0b4
DiegoGary <diego.garcia04@upaep.edu.mx>
2025-08-26 10:30:27 -0600

    no message

---
+++ js/effects.js
@@ -0,0 +1,31 @@
+var playfieldEl, vignetteEl, announceEl;
+
+function initEffects(){
+  playfieldEl=document.getElementById('playfield');
+  vignetteEl=document.getElementById('vignette');
+  announceEl=document.getElementById('announcements');
+}
+
+function shake(){
+  playfieldEl.classList.add('shake');
+  setTimeout(function(){playfieldEl.classList.remove('shake');},180);
+}
+
+function flashRed(){
+  vignetteEl.classList.add('red');
+  setTimeout(function(){vignetteEl.classList.remove('red');},350);
+}
+
+function setBlueActive(a){
+  if(a){vignetteEl.classList.add('blue');}
+  else{vignetteEl.classList.remove('blue');}
+  if(!a) play('freeze_off');
+}
+
+function banner(text){
+  var div=document.createElement('div');
+  div.className='banner';
+  div.textContent=text;
+  announceEl.appendChild(div);
+  setTimeout(function(){if(div.parentNode) announceEl.removeChild(div);},900);
+}
